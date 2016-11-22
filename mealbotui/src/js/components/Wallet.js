6bcfb48c027162bcf524e69efed5d966dd2c40a2
qjflores <quijanoflores@gmail.com>
2016-11-22 22:36:05 +0530

    working add wallet feature

--- mealbotui/src/js/components/Wallet.js
+++ mealbotui/src/js/components/Wallet.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
 
+import {AddWalletModal} from './AddWalletModal';
+
 import './../../css/Wallet.css'
 
 export class Wallet extends Component {
   constructor(props) {
     super(props);
-    this.state = {isWalletAttached: false,
-      showModal: false};
+    this.state = {
+      isWalletAttached: false,
+      showModal: false,
+      wallets:[]
+    };
     this.attachWallet = this.attachWallet.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.addWallet = this.addWallet.bind(this);
   }
 
   componentWillMount() {
-    this.setState({isWalletAttached:this.props.isWalletAttached})
+    this.setState({
+      isWalletAttached:this.props.isWalletAttached,
+      wallets:[{address:"0x1",name:"wallet-1"}, {address:"0x2",name:"wallet-2"}]
+    })
+  }
+
+  closeModal() {
+    console.log("closeModal");
+    this.setState({ showModal: false });
+  }
+
+  openModal() {
+    this.setState({ showModal: true });
+    console.log("open");
   }
 
   attachWallet() {
+    this.openModal();
     console.log("attachWallet");
   }
 
-  //TODO move wallet list into its own component
+  addWallet(walletName, walletAddress) {
+    var updatedWallet =this.state.wallets;
+    updatedWallet.push({address:walletAddress,name:walletName});
+    this.setState({ showModal: false, wallets:updatedWallet});
+  }
+
   render() {
-    var wallets = [
-      {pk:1,address:"0x1",name:"wallet-1"}, {pk:2,address:"0x2",name:"wallet-2"}
-      ];
+    var wallets = this.state.wallets;
     var walletList = wallets.map(function(wallet) {
       return <div className="wallet-list">{wallet.address} {wallet.name}</div>
       })
@@ -36,6 +61,12 @@ export class Wallet extends Component {
         <i className="fa fa-id-card wallet-icon"></i>
         Attach Wallet
       </div>
+      {this.state.showModal ? <AddWalletModal 
+        showModal={this.state.showModal} 
+        closeModal={this.closeModal}
+        addWallet={this.addWallet}
+        /> : null}
+
       </div>
       )
   }
